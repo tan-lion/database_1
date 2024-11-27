6205eeb8cc8ca682536ed63f687126d1909dc203
tai-lion <tanboss159@gmail.com>
2024-11-27 23:58:35 +0700

    push file sql

---
+++ students_202411272354.sql
@@ -0,0 +1,8 @@
+INSERT INTO student.students (full_name,gender,age,city,weight) VALUES
+	 ('Nguyen Thanh Nhan','Nam',19,'Can Tho',56.5674),
+	 ('Pham Thu Huong','Nu',20,'Vinh Long',72.456),
+	 ('Nguyen Nhu Ngoc','Nu',20,'Soc Trang',85.387),
+	 ('Bui Thanh Bao','Nam',19,'Soc Trang',49.3),
+	 ('Le My Nhan','Nu',22,'Can Tho',62.963),
+	 ('Tan Thuc Bao','Nam',35,'An Giang',55.5678),
+	 ('Trinh Giao Kim','Nam',44,'Bac Lieu',67.34);
